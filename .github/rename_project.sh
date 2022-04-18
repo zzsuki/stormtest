827dd75016b30f1c64196e11ad482f9db6621533
zzsuki <zzsuki@users.noreply.github.com>
2022-04-18 02:15:13 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="zzsuki"
+original_name="stormtest"
+original_urlname="stormtest"
+original_description="Awesome stormtest created by zzsuki"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv stormtest $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
